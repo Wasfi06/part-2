3130ef1eea029f12b9ee8681b54bdb66c39f9392
Wasfi06 <ST10477266@rcconnect.edu.za>
2026-09-02 21:44:48 +0200

    Update 20260902092717_InitialCreate.cs

--- RaceDay/Migrations/20260902092717_InitialCreate.cs
+++ RaceDay/Migrations/20260902092717_InitialCreate.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace RaceDay.Migrations
 {
-    /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -223,8 +221,6 @@ namespace RaceDay.Migrations
                 column: "Email",
                 unique: true);
         }
-
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
